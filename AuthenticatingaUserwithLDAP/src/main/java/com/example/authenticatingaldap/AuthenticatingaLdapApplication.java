a960686e7b471a502cbd0f6b00281c610629b0f4
주상현 <monayo.jsh@gmail.com>
2023-04-21 09:43:15 +0900

    AuthenticatingaUserwithLDAP

---
+++ AuthenticatingaUserwithLDAP/src/main/java/com/example/authenticatingaldap/AuthenticatingaLdapApplication.java
@@ -0,0 +1,13 @@
+package com.example.authenticatingaldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticatingaLdapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticatingaLdapApplication.class, args);
+    }
+
+}
---
+++ AuthenticatingaUserwithLDAP/src/test/java/com/example/authenticatingaldap/AuthenticatingaLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authenticatingaldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticatingaLdapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
